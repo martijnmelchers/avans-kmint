849840c8bbae4531cbe4937a3d27598625ef04b4
Martijn <martijn@melchers.nu>
2021-01-10 15:41:47 +0100

    zooi

--- pigisland/src/kmint/pigisland/pig.cpp
+++ pigisland/src/kmint/pigisland/pig.cpp
@@ -4,47 +4,43 @@
 namespace kmint::pigisland {
 
     pig::pig(math::vector2d location)
-            :PhysicsActor(location, drawable_),
-              drawable_{*this, pig_image()}  {
+            : PhysicsActor(location, drawable_),
+              drawable_{*this, pig_image()} {
         _position = location;
     }
 
     void pig::act(delta_time dt) {
 
-        update_surrounding();
-
-        vector2d steeringForce  = _steeringBehaviour->calculate();
-        vector2d acceleration = steeringForce / mass;
-        _velocity += acceleration * dt.count();
-        _velocity.truncate(max_speed);
-
-        _position += _velocity * to_seconds(dt);
-
-        location(_position);
-
-        auto length = (_velocity.x() * _velocity.x() + _velocity.y() * _velocity.y());
-
-
-        if(length > 0.00000001){
-            heading(math::normalize(_velocity));
-        }
-
-
-        for (std::size_t i = 0; i < num_colliding_actors(); ++i) {
-            auto &a = colliding_actor(i);
-
-
-
-            if (boat_ != nullptr && &a == boat_) {
+        this->handle_collisions([this](actor &a) {
+            if (auto *boat = dynamic_cast<kmint::pigisland::boat *>(&a)) {
                 std::cout << "COLLIDED WITH BOAT!!!" << std::endl;
                 this->remove();
-            }
-
-            if (shark_ != nullptr && &a == shark_) {
+            } else if (auto *shark = dynamic_cast<kmint::pigisland::shark *>(&a)) {
                 std::cout << "COLLIDED WITH SHARK!!!" << std::endl;
                 this->remove();
             }
+        });
+
+        if (!this->removed()) {
+            update_surrounding();
+
+            vector2d steeringForce = _steeringBehaviour->calculate();
+            vector2d acceleration = steeringForce / mass;
+            _velocity += acceleration * dt.count();
+            _velocity.truncate(max_speed);
+
+            _position += _velocity * to_seconds(dt);
+
+            location(_position);
+
+            auto length = (_velocity.x() * _velocity.x() + _velocity.y() * _velocity.y());
+
+
+            if (length > 0.00000001) {
+                heading(math::normalize(_velocity));
+            }
         }
+
 
     }
 
